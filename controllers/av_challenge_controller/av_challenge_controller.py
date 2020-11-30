256344a551e28a800fa67a312e3410e8b87cc603
greg-lund <grlu8224@colorado.edu>
2020-11-29 20:18:39 -0700

    Line follower controller

--- controllers/av_challenge_controller/av_challenge_controller.py
+++ controllers/av_challenge_controller/av_challenge_controller.py
@@ -2,38 +2,98 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot, Camera
+from controller import Robot, Camera, Motor, Lidar
+from vehicle import Driver
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def laneLineCmdVel(camera,white_sensitivity=60):
+    '''
+    Take in raw image from front_camera, track 
+    lane lines and return steering angle such that
+    the car centers itself on the lines
+    '''
+
+    # Get image from camera in the correct orientation
+    img = cv2.flip(cv2.rotate(np.array(camera.getImageArray(), dtype='uint8'),cv2.ROTATE_90_CLOCKWISE),1)
+
+    # Mask image to get just the lane line
+    img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    lower_white = np.array([0,0,255-white_sensitivity],dtype=np.uint8)
+    upper_white = np.array([255,white_sensitivity,255],dtype=np.uint8)
+    mask = cv2.inRange(img_hsv,lower_white,upper_white)
+
+    # Get edges via canny edge detection
+    edges = cv2.Canny(mask,200,400)
+    (h,w) = edges.shape
+    edges[0:int(2*h/3)][:] = 0
+
+    # Convert edges into lines via Hough Transform
+    lines = cv2.HoughLinesP(edges,1,np.pi/180,1,np.array([]),minLineLength=8,maxLineGap=24)
+
+    # If no lines return no steering command
+    if lines is None:
+        return 0
+
+    # Otherwise lets average, find the slope and displacement from center
+    avg_center = 0
+    avg_theta = 0
+    n = 0
+    for l in lines:
+        l = l.flatten()
+        if l[1] > l[3]:
+            theta = np.pi - np.arctan2(l[1]-l[3],l[0]-l[2])
+        else:
+            theta = np.pi - np.arctan2(l[3]-l[1],l[2]-l[0])
+        avg_theta += theta
+        avg_center += (l[0]+l[2])/2
+        n += 1
+
+    avg_center /= n
+    avg_theta /= n
+
+    cv2.imshow('image',edges)
+    cv2.waitKey(1)
+
+    print("avg_center:",avg_center)
+    print("center:",(avg_center-w/2)/(w/2))
+    return (avg_center - w/2)/(w/2)
+    #return np.pi/2 - avg_theta
+
+def getFrame(camera):
+    img = np.array(camera.getImageArray(), dtype='uint8')
+    img = cv2.flip(cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE),1)
+    return img
 
 # create the Robot instance.
-robot = Robot()
-front_camera = robot.getCamera("front_camera")
-rear_camera = robot.getCamera("rear_camera")
+driver = Driver()
+front_camera = driver.getCamera("front_camera")
+rear_camera = driver.getCamera("rear_camera")
+lidar = driver.getLidar("Sick LMS 291")
 
 # get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+timestep = int(driver.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getMotor('motorname')
-#  ds = robot.getDistanceSensor('dsname')
-#  ds.enable(timestep)
+front_camera.enable(100)
+rear_camera.enable(100)
+lidar.enable(100)
+lidar.enablePointCloud()
 
+driver.setCruisingSpeed(10)
 
-front_camera.enable(30)
-rear_camera.enable(30)
-
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
+max_delta = 5 * np.pi / 180
+while driver.step() != -1:
+    curr_angle = driver.getSteeringAngle()
+    cmd_angle = laneLineCmdVel(front_camera,60)
+    '''
+    if abs(cmd_angle - curr_angle) > max_delta:
+        if cmd_angle > curr_angle:
+            cmd_angle = min(np.pi/2,curr_angle + max_delta)
+        else:
+            cmd_angle = max(-np.pi/2,curr_angle - max_delta)
+    '''
+    cmd_angle /= np.pi/2
+    print('curr_angle: %f, cmd_angle: %f'%(curr_angle,cmd_angle))
+    driver.setSteeringAngle(cmd_angle)
 
